e716f75396f8f71d3a7fc0dd5842f62cd7f8872c
ddelis2016-sys <ddelis2016@gmail.com>
2026-04-29 10:41:41 -0400

    menu

---
+++ Assets/LaunchGame.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchGame : MonoBehaviour
+{
+    public void LoadTheGameScene()
+    {
+        SceneManager.LoadScene("midterrm");
+
+    }
+
+ 
+}
